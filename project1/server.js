3608ae69048e0be5bb092e5f2be107830ad9c139
@vaishali.pandey <pvaishali@gmail.com>
2024-09-05 19:25:53 +0530

    commit

--- project1/server.js
+++ project1/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get('/project1', (req, res) => {
   res.send('Hello from Project 1!');
 });
 
